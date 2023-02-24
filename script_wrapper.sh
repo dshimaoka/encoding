ab03e87ea57dbd35c8bac2425d5eb661acb0af94
Daisuke <sageagainstthemachine@gmail.com>
2023-02-24 22:14:03 +1100

    120 arrays

--- script_wrapper.sh
+++ script_wrapper.sh
@@ -8,7 +8,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=2
 #SBATCH --mem-per-cpu=33000
-#SBATCH --array=1-20
+#SBATCH --array=1-120
 #SBATCH --gres=gpu:1
 #SBATCH --partition=m3h
 module load matlab
