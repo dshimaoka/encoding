df5e6ae07c9b33a83a319ca806d44c89609306b1
Daisuke <sageagainstthemachine@gmail.com>
2023-02-28 14:07:32 +1100

    mod .sh

--- script_wrapper.sh
+++ script_wrapper.sh
@@ -3,8 +3,8 @@
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
 #SBATCH --mail-user=daisuke.shimaoka@monash.edu
-#SBATCH --job-name=Wrapper_430arrays
-#SBATCH --time=55:00:00
+#SBATCH --job-name=Wrapper_5arrays
+#SBATCH --time=05:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=33000
