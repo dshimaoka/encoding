4271b3fa61893ec31039a681c728f75d3fc2b9c1
Daisuke <sageagainstthemachine@gmail.com>
2023-03-07 22:40:33 +1100

    cpu1 for test

--- script_summary.sh
+++ script_summary.sh
@@ -6,7 +6,7 @@
 #SBATCH --job-name=Summary
 #SBATCH --time=24:00:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=6
+#SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=12000
 #SBATCH --gres=gpu:1
 #SBATCH --partition=m3h
