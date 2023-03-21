f685d10fe4d015dd86224b0fdab3da6b6c797495
Daisuke <sageagainstthemachine@gmail.com>
2023-03-22 05:55:27 +1100

    60arrays ID3 noxval 3rd trial

--- script_wrapper_nxv.sh
+++ script_wrapper_nxv.sh
@@ -3,12 +3,12 @@
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
 #SBATCH --mail-user=daisuke.shimaoka@monash.edu
-#SBATCH --job-name=Wrapper_nxv_5arrays
+#SBATCH --job-name=Wrapper_nxv_60arrays
 #SBATCH --time=09:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=33000
-#SBATCH --array=1-5
+#SBATCH --array=1-60
 #SBATCH --gres=gpu:1
 #SBATCH --partition=m3g
 module load matlab
