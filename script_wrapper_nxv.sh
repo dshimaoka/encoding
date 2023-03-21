5b4eea7bbab940d09e5c53f5f2424eac525ea4f2
Daisuke <sageagainstthemachine@gmail.com>
2023-03-22 05:23:40 +1100

    5arraysID2 nxval 3rd trial

--- script_wrapper_nxv.sh
+++ script_wrapper_nxv.sh
@@ -3,12 +3,12 @@
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
 #SBATCH --mail-user=daisuke.shimaoka@monash.edu
-#SBATCH --job-name=Wrapper_nxv_1000arrays
-#SBATCH --time=29:00:00
+#SBATCH --job-name=Wrapper_nxv_5arrays
+#SBATCH --time=09:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=33000
-#SBATCH --array=1-1000
+#SBATCH --array=1-5
 #SBATCH --gres=gpu:1
 #SBATCH --partition=m3g
 module load matlab
