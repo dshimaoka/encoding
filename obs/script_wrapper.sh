9e9a145c4ae4c90865681a35b0b3a16ac0a23e53
Daisuke <sageagainstthemachine@gmail.com>
2023-03-22 11:47:44 +1100

    merge files for nxv with original

--- obs/script_wrapper.sh
+++ obs/script_wrapper.sh
@@ -3,13 +3,13 @@
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
 #SBATCH --mail-user=daisuke.shimaoka@monash.edu
-#SBATCH --job-name=Wrapper_nxv_60arrays
-#SBATCH --time=09:00:00
+#SBATCH --job-name=Wrapper_42arrays
+#SBATCH --time=99:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=33000
-#SBATCH --array=1-60
+#SBATCH --array=1-1000
 #SBATCH --gres=gpu:1
 #SBATCH --partition=m3g
 module load matlab
-matlab -nodisplay -nodesktop -nosplash < wrapper_encoding_nxv.m
+matlab -nodisplay -nodesktop -nosplash < wrapper_encoding.m
