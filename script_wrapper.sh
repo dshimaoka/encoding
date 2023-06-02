499c5bc655f537f01a1c6a77c2f12851edb37f08
Daisuke <sageagainstthemachine@gmail.com>
2023-06-03 09:09:47 +1000

    switch to m3g

--- script_wrapper.sh
+++ script_wrapper.sh
@@ -8,8 +8,8 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=33000
-#SBATCH --array=1-10
+#SBATCH --array=1-5
 #SBATCH --gres=gpu:1
-#SBATCH --partition=m3h
+#SBATCH --partition=m3g
 module load matlab
 matlab -nodisplay -nodesktop -nosplash < wrapper_encoding.m
