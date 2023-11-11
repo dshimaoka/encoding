a5b5f389a932a98f073692bfae63e95c74e3c627
Daisuke <sageagainstthemachine@gmail.com>
2023-11-11 15:49:31 +1100

    m3u
    as m3h faced the error: "sbatch: error: invalid partition specified: m3h
    sbatch: error: Batch job submission failed: Invalid partition name specified"

--- script_wrapper_spd.sh
+++ script_wrapper_spd.sh
@@ -10,6 +10,6 @@
 #SBATCH --mem-per-cpu=80000
 #SBATCH --array=1-1
 #SBATCH --gres=gpu:1
-#SBATCH --partition=m3h
+#SBATCH --partition=m3u
 module load matlab
 matlab -nodisplay -nodesktop -nosplash < wrapper_encoding_spd.m
