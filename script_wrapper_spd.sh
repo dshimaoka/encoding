db52865d0c59bd0b7f496e4ff773cde0a8201418
Daisuke <sageagainstthemachine@gmail.com>
2023-11-13 09:57:56 +1100

    asking gpu partition

--- script_wrapper_spd.sh
+++ script_wrapper_spd.sh
@@ -10,6 +10,6 @@
 #SBATCH --mem-per-cpu=80000
 #SBATCH --array=1-1
 #SBATCH --gres=gpu:1
-#SBATCH --partition=m3f
+#SBATCH --partition=gpu
 module load matlab
 matlab -nodisplay -nodesktop -nosplash < wrapper_encoding_spd.m
