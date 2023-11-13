2aecc9faa6eff44831683bad6b93789d985709a1
Daisuke Shimaoka <daisuke.shimaoka@monash.edu>
2023-11-13 18:37:19 +1100

    asking 10 arrays

--- script_makeStimDataBase.sh
+++ script_makeStimDataBase.sh
@@ -4,11 +4,11 @@
 #SBATCH --mail-type=END
 #SBATCH --mail-user=daisuke.shimaoka@monash.edu
 #SBATCH --job-name=Wrapper
-#SBATCH --time=20:00:00
+#SBATCH --time=15:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=130000
-#SBATCH --array=1-5
+#SBATCH --array=1-10
 module load matlab/r2021a
 module load gst-libav
 matlab -nodisplay -nodesktop -nosplash < makeStimDataBase.m
